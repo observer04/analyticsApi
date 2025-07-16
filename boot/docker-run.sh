ba3c1293d1e364e6c790402d515f6670d97ed327
observer04 <tripathysarojinee@gmail.com>
2025-07-16 14:40:23 +0530

    compose changes on working dir

--- boot/docker-run.sh
+++ boot/docker-run.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
-source /opt/venv/bin/activate
+# activate venv
+[ -f /opt/venv/bin/activate ] && source /opt/venv/bin/activate
 
+# where main.py lives
 cd /code
 
-RUN_PORT=${PORT:-8000}
 RUN_HOST=${HOST:-0.0.0.0}
+RUN_PORT=${PORT:-8000}
 
-gunicorn -k uvicorn.workers.UvicornWorker \
-   --bind "${RUN_HOST}:${RUN_PORT}" \
-   main:app
+
+exec gunicorn -k uvicorn.workers.UvicornWorker \
+     --bind "${RUN_HOST}:${RUN_PORT}" \
+     main:app
